3081a07a904b7bc5997ed466cd9d51a50ff8f8f0
Amir Tanvir <amirtanvir96@gmail.com>
2023-01-22 21:02:24 +0000

    next auth v2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  appDir: true,
+  experimental: {
+    appDir: true,
+  },
 }
 
-module.exports = {
-  images: {
-    domains: ["links.papareact.com","fakestoreapi.com", './src/assets/products/']
-  }
-}
+module.exports = nextConfig
