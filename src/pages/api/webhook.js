857dd1d13469d83b43bb9923e0cc7069cbbc4001
Amir Tanvir <amirtanvir96@gmail.com>
2023-01-24 22:44:34 +0000

    firebase is now working

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,57 @@
+import { buffer } from 'micro'
+import * as admin from 'firebase-admin'
+
+
+
+const serviceAccount = require('../../../permissions.json')
+const app = !admin.apps.length ? admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+}) : admin.app();
+
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fulfillOrder = async (session) => {
+    console.log('Fulfilling order')
+
+    return app.firestore().collection('users').doc(session.metadata.email).collection('orders').doc(session.id).set({
+        amount: session.amount_total / 100,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+        console.log("SUCCESS: Order has been added to the database")
+    })
+}
+
+export default async (req,res) => {
+    if (req.method === 'POST'){
+        const requestBuffer = await buffer(req);
+        const payload = requestBuffer.toString();
+        const sig = req.headers["stripe-signature"]
+
+        let event;
+
+        try {
+            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
+            console.log(event)
+        } catch (err) {
+            return res.status(400).send(`Webhook error: ${err.message}`)
+        }
+
+        if (event.type === 'checkout.session.completed') {
+            const session = event.data.object;
+
+            return fulfillOrder(session)
+                .then(() => res.status(200))
+                .catch(err => res.status(400).send(`Webhook error: ${err.message}`))
+        }
+    }
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true
+    }
+}
