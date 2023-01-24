82936f60d027d9486c9571241b3f32f171862ad9
Amir Tanvir <amirtanvir96@gmail.com>
2023-01-24 22:01:57 +0000

    stripe is now working

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -4,31 +4,23 @@ export default async (req, res) => {
     const { items, email } = req.body;
 
     const transformedItems = items.map(item => ({
-        description: item.description,
-        quantity: 1,
         price_data: {
             currency: 'gbp',
             unit_amount: item.price * 100,
             product_data: {
                 name: item.title,
-                images: [item.image],
             },
-        }
+        },
+        quantity: 1,
     }));
 
     const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        shipping_rates: ['shr_1MTMdTGABRBPlaX7gmlk5djv'],
-        shipping_address_collection: {
-            allowed_countries: ['GB', 'US', 'CA']
-        },
         line_items: transformedItems,
         mode: 'payment',
         success_url: `${process.env.HOST}/success`,
         cancel_url: `${process.env.HOST}/checkout`,
         metadata: {
             email,
-            images: JSON.stringify(items.map(item => item.image))
         }
     });
 
